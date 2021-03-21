bf54c65b8bdcbdb3309492448d956965ef2ff98d
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2021-03-21 23:52:21 +0000

    Delete sv_antiscreengrabdetour.lua

--- lua/bsecure/modules/anticheatprecautions/sv_antiscreengrabdetour.lua
+++
@@ -1,6 +0,0 @@
-util.AddNetworkString("bSecure.DetouredScreengrabDetected")
-net.Receive("bSecure.DetouredScreengrabDetected", function(_,pPlayer)
-    bSecure.PrintDetection(pPlayer:Nick() .. " - Detected detoured screengrab function(s).")
-    hook.Run("bSecure.DetectedDetouredScreengrab", pPlayer)
-    bSecure.BanPlayer(pPlayer,"Detoured screengrab", 0)
-end)
