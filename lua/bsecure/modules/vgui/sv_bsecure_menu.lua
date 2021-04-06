11d4b8616dc6210be9f41d4540274f66bbe8b616
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2021-04-06 19:23:09 +0100

    Chat Command
    added a chat command since console commands dont work in multiplayer servers for some reason

--- lua/bsecure/modules/vgui/sv_bsecure_menu.lua
+++ lua/bsecure/modules/vgui/sv_bsecure_menu.lua
@@ -5,3 +5,12 @@ bSecure.ConCommandAdd("menu", function(ply)
     net.Start("bSecure.OpenMenu")
     net.Send(ply)
 end)
+
+hook.Add("PlayerSay", "bSecure.OpenMenu", function(pPlayer, strText)
+    if not strText:lower() == "!bsecure" then return end
+    if not ply:IsAdmin() then return bSecure.ChatPrint(ply, "Insufficient access") end
+    bSecure.ChatPrint(ply, "Opening the menu...")
+    net.Start("bSecure.OpenMenu")
+    net.Send(ply)
+    return ""
+end)
