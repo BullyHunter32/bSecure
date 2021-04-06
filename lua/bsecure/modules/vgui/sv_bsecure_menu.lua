016ad41e5bb69c7f69b83ed6c961c79b6ec06a99
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2021-04-06 19:23:43 +0100

    added chat command

--- lua/bsecure/modules/vgui/sv_bsecure_menu.lua
+++ lua/bsecure/modules/vgui/sv_bsecure_menu.lua
@@ -8,9 +8,9 @@ end)
 
 hook.Add("PlayerSay", "bSecure.OpenMenu", function(pPlayer, strText)
     if not strText:lower() == "!bsecure" then return end
-    if not ply:IsAdmin() then return bSecure.ChatPrint(ply, "Insufficient access") end
-    bSecure.ChatPrint(ply, "Opening the menu...")
+    if not pPlayer:IsAdmin() then return bSecure.ChatPrint(pPlayer, "Insufficient access") end
+    bSecure.ChatPrint(pPlayer, "Opening the menu...")
     net.Start("bSecure.OpenMenu")
-    net.Send(ply)
+    net.Send(pPlayere)
     return ""
 end)
