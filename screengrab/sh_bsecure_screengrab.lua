2a12fa1942da11687c41b8a626375198805f0dda
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2021-06-07 20:53:20 +0100

    Delete screengrab directory

--- screengrab/sh_bsecure_screengrab.lua
+++
@@ -1,8 +0,0 @@
-if not file.Exists("bsecure/screengrabs", "DATA") then
-    file.CreateDir("bsecure/screengrabs")
-end
-
-if not file.Exists("bsecure/screengrabs/"..(CLIENT and "client" or "server"), "DATA") then
-    file.CreateDir("bsecure/screengrabs/"..(CLIENT and "client" or "server"))
-end
-
